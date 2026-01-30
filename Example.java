ac4dfba639d97f94fbee9a8f1681b8a51ff29d31
sanjana <sanjanambwork@gmail.com>
2026-01-30 19:44:27 +0530

    feat : add login feature

--- Example.java
+++ Example.java
@@ -1,5 +1,6 @@
 public class Example{
     public static void main(String args[]){
         System.out.println("Project : Hospital Management System");
+        System.out.println("Login feature");
     }
 }
