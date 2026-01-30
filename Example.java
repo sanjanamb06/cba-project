8843778296f20dbb5264ff5e4a926ba6b9a70de6
sanjana <sanjanambwork@gmail.com>
2026-01-30 21:50:25 +0530

    Last commit

--- Example.java
+++ Example.java
@@ -2,6 +2,6 @@ public class Example{
     public static void main(String args[]){
         System.out.println("Project : Hospital Management System");
         int y=20;
-        
+        int x = 500;
     }
 }
