9de816974bebfd446518e148b58627b10a824ca2
sanjana <sanjanambwork@gmail.com>
2026-01-30 19:50:22 +0530

    edit from main

--- Example.java
+++ Example.java
@@ -1,6 +1,7 @@
 public class Example{
     public static void main(String args[]){
         System.out.println("Project : Hospital Management System");
-        System.out.println("Login feature");
+        System.out.println("Login feature being edited");
+        
     }
 }
