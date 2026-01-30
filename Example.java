e1b502d52570abf90194d39796255ff1f42ea1cb
sanjana <sanjanambwork@gmail.com>
2026-01-30 20:01:20 +0530

    main : changed y to 20

--- Example.java
+++ Example.java
@@ -1,7 +1,7 @@
 public class Example{
     public static void main(String args[]){
         System.out.println("Project : Hospital Management System");
-        int y=10;
+        int y=20;
         
     }
 }
